aa2730378dc24479d5fb84c718c698eed3c5978f
vishnuvardhan938121 <147582100+vishnuvardhan938121@users.noreply.github.com>
2025-10-04 15:31:34 +0530

    Update EditorPage.js

--- client/src/components/EditorPage.js
+++ client/src/components/EditorPage.js
@@ -1,5 +1,5 @@
   
-  import React, { useEffect, useRef, useState } from "react";
+  import React, { useEffect, useRef, useState } from "react"; 
 import Client from "./Client";
 import Editor from "./Editor";
 import { initSocket } from "../Socket";
