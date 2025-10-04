6f9404d71ec7e69fbcbcf99956d81d54566dd300
vishnuvardhan938121 <147582100+vishnuvardhan938121@users.noreply.github.com>
2025-10-04 15:41:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -94,24 +94,7 @@ io.on("connection", (socket) => {
   });
 });
 
-/*app.post("/compile", async (req, res) => {
-  const { code, language } = req.body;
-
-  try {
-    const response = await axios.post("https://api.jdoodle.com/v1/execute", {
-      script: code,
-      language: language,
-      versionIndex: languageConfig[language].versionIndex,
-      clientId: process.env.jDoodle_clientId,
-      clientSecret: process.env.jDoodle_clientSecret,
-    });
-
-    res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to compile code" });
-  }
-});  */       
+     
 
 app.post("/compile", async (req, res) => {
   const { code, language } = req.body;
